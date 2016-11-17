8e5ebb6835df3a3973ad1e6ddcf16c96f4e2ed9c
kseniyavaleeva <valeeva_ksushka_1997@mail.ru>
2016-11-17 16:50:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+﻿(function(){
+	
+	window.App = {
+		Views: {},
+		Models: {},
+		Collections : {},
+		Router:{}
+	};
+	
+	
+	window.template = function(id){
+        return _.template($('#' + id).html());
+};
+
+App.Router = Backbone.Router.extend({
+   routes:{
+	'' : 'index',
+	'read': 'read'
+   },
+
+     index: function(){
+          console.log('Всем привет от индексного роута!');
+   },
+   
+   
+   read: function(){
+	    console.log('Это роут Read');
+   }
+});
+
+new App.Router();
+Backbone.history.start();
+
+
+})();
